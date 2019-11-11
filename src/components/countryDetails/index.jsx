7d32b6aa1f19c9c459f7981e5f5c661aea16068f
Gabriel Oliveira <g.webdevelopr@gmail.com>
2019-11-11 16:23:53 -0300

    routing and recovering data fetched

--- src/components/countryDetails/index.jsx
+++ src/components/countryDetails/index.jsx
@@ -1,27 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouteMatch } from "react-router-dom";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 export default function CountryDetails(props) {
     const match = useRouteMatch("/:id")
+    
+    const [country, setCountry] = useState([]);
+    const abortController = AbortController();
+    const signal = abortController.signal;
+
 
     useEffect(() => {
         getCountryDetails();
     });
-
+        
     const getCountryDetails = async () => {
-        const data = await fetch(`https://restcountries.eu/rest/v2/alpha/${match.params.id}`)
+        const data = await fetch(`https://restcountries.eu/rest/v2/alpha/${match.params.id}`, {signal: signal})
+        const item  = await data.json();
 
-        const country = await data.json()
+        setCountry(item);    
 
-        console.log(country)
-
+        return function cleanup() {
+            abortController.abort()
+        }
     }
 
+
+
+    
     return (
         <div id={"details"}>
-            <h1>This is details for {match.params.id}</h1>     
+            <Link to={"/"}>
+                <div id={"button-to-home"}>Back</div>
+            </Link>
+            <h1>{country.name}</h1>     
         </div>
     )
 }
-
