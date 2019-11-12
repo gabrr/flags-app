3a2ffbd5f1c0a5e8d2a526719189eefe2f0f42c0
Gabriel Oliveira <g.webdevelopr@gmail.com>
2019-11-12 20:21:06 -0300

    background color difference fixed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -36,12 +36,14 @@ class Navbar extends Component {
 
     navbarBackground() {
         if(store.getState().theming === "light-theme") {
+            document.body.style.backgroundColor = store.getState().lightTheme.elements
             return {
                 backgroundColor: store.getState().lightTheme.elements, 
                 transition: "background-color 200ms linear 20ms",
                 color: store.getState().lightTheme.text
             }
         } else {
+            document.body.style.backgroundColor = store.getState().darkTheme.elements
             return {
                 backgroundColor: store.getState().darkTheme.elements, 
                 transition: "background-color 200ms linear 20ms",
